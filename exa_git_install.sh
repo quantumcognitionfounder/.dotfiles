e84608fbc32e690606adc4f0b228f2794b9e2cc3
quantumcognitionfounder <whenidieputmymoneyinacave@gmail.com>
2024-03-09 17:23:35 -0500

    typos

--- exa_git_install.sh
+++ exa_git_install.sh
@@ -8,10 +8,10 @@ mkdir /tmp/exa
 # Unzip exa
 unzip exa-linux-x86_64-v0.10.0.zip -d /tmp/exa
 
-cp /tmp/exa/exa /usr/local/bin
+cp /tmp/exa/bin/exa /usr/local/bin
 
-cp /tmp/completions/exa.zsh /usr/local/share/zsh/site-functions/_exa
+cp /tmp/exa/completions/exa.zsh /usr/local/share/zsh/site-functions/_exa
 
-cp /tmp/man/exa.1 /usr/share/man/man1
+cp /tmp/exa/man/exa.1 /usr/share/man/man1
 
-cp /tmp/man/exa /usr/share/man/man5/exa_colors.5
+cp /tmp/exa/man/exa_colors.5 /usr/share/man/man5
